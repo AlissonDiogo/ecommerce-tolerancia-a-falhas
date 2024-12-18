50b16770d621833e33276f8113a524f6fbd5871c
Pedro Medeiros <pedro.figueiredo.114@ufrn.edu.br>
2024-12-18 18:38:12 -0300

    feat: implementa cliente http para consumir micro serviços

---
+++ ecommerce/src/main/java/br/ufrn/imd/ecommerce/client/WebClient.java
@@ -0,0 +1,53 @@
+package br.ufrn.imd.ecommerce.client;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class WebClient {
+
+    private String address;
+
+    private HttpClient httpClient;
+    private ObjectMapper objectMapper;
+
+    public WebClient(String address) {
+        this.httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+        this.objectMapper = new ObjectMapper();
+        this.address = address;
+    }
+
+    public Object post(String endpoint, Object body) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(address.concat(endpoint)))
+                .version(HttpClient.Version.HTTP_2)
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(body)))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        return objectMapper.readValue(response.body(), Object.class);
+    }
+
+    public <T> T get(String endpoint, Class<T> responseType) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(address.concat(endpoint)))
+                .version(HttpClient.Version.HTTP_2)
+                .header("Accept", "application/json")
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("Falha ao consumir endpoint:" + endpoint + ", status code: " + response.statusCode());
+        }
+
+        return objectMapper.readValue(response.body(), responseType);
+    }
+}
