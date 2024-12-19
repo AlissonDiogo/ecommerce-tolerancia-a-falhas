0e141f3ceac47f1d0a2e9cc4a667429abb047c8d
Pedro Medeiros <pedro.figueiredo.114@ufrn.edu.br>
2024-12-19 17:09:53 -0300

    feat: adiciona chamada do endpoint /sell no serviço do e-ecommerce

--- ecommerce/src/main/java/br/ufrn/imd/ecommerce/service/store/StoreService.java
+++ ecommerce/src/main/java/br/ufrn/imd/ecommerce/service/store/StoreService.java
@@ -1,9 +1,15 @@
 package br.ufrn.imd.ecommerce.service.store;
 
 import br.ufrn.imd.ecommerce.dto.ProductResponseDto;
+import br.ufrn.imd.ecommerce.dto.SellRequestDto;
+import br.ufrn.imd.ecommerce.utils.fails.Fail;
+
+import java.util.UUID;
 
 public interface StoreService {
 
-    ProductResponseDto checkProductById(String productId);
+    ProductResponseDto checkProductById(String productId) throws Fail;
+
+    UUID sellProduct(SellRequestDto requestDto) throws Fail;
 
 }
--- ecommerce/src/main/java/br/ufrn/imd/ecommerce/service/store/StoreServiceImpl.java
+++ ecommerce/src/main/java/br/ufrn/imd/ecommerce/service/store/StoreServiceImpl.java
@@ -1,24 +1,46 @@
 package br.ufrn.imd.ecommerce.service.store;
 
-import br.ufrn.imd.ecommerce.client.WebClient;
+import br.ufrn.imd.ecommerce.dto.SellRequestDto;
+import br.ufrn.imd.ecommerce.utils.client.RestClient;
 import br.ufrn.imd.ecommerce.dto.ProductResponseDto;
+import br.ufrn.imd.ecommerce.utils.fails.Fail;
+import br.ufrn.imd.ecommerce.utils.fails.FailType;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.net.http.HttpResponse;
+import java.util.UUID;
 
 public class StoreServiceImpl implements StoreService {
 
-    private final WebClient webClient;
+    private final RestClient restClient;
 
     public StoreServiceImpl(String address) {
-        this.webClient = new WebClient(address);
+        this.restClient = new RestClient(address);
     }
 
     @Override
-    public ProductResponseDto checkProductById(String productId) {
+    public ProductResponseDto checkProductById(String productId) throws Fail {
         try{
-            return webClient.get("/store/product?productId="+productId, ProductResponseDto.class);
+            return restClient.get("/store/product?productId="+productId, ProductResponseDto.class);
         } catch (IOException | InterruptedException e) {
-            throw new RuntimeException("Erro no processamento da consulta do produto: " + e.getMessage());
+            throw new Fail(FailType.OMISSION);
         }
     }
+
+    @Override
+    public UUID sellProduct(SellRequestDto requestDto) throws Fail {
+        record SellResponseDto(UUID transactionId){}
+
+        try{
+            HttpResponse<String> response = restClient.post("/store/sell", requestDto);
+            SellResponseDto responseDto = new ObjectMapper().readValue(response.body(), SellResponseDto.class);
+
+            return responseDto.transactionId();
+        }catch (IOException | InterruptedException f) {
+            throw new Fail(FailType.ERROR);
+        }
+    }
+
+
 }
